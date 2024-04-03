b2efca3fb109c149e3b69929d723d39487834c88
Abo7med177 <101987378+Abo7med177@users.noreply.github.com>
2024-04-04 02:42:04 +0300

    up

---
+++ events/mangementPoint.js
@@ -0,0 +1,86 @@
+// تعاريف :
+const { Database } = require("npm.db")
+const db = new Database("database.json")
+const client = require("discord.js")
+const {MessageEmbed,MessageActionRow,MessageButton} = require("discord.js")
+const mangement = "1117855649720709181"
+const prefix = "#"
+const perm = "1123963696415260744"
+// النقاط التلقائية : 
+client.on("channelCreate", async ch => {
+    if(ch.name.startsWith("ticket-")){ 
+        setTimeout(() => {
+            ch.send({
+                components: [new MessageActionRow().addComponents(new MessageButton().setCustomId("claim").setLabel("استلام").setStyle("PRIMARY"))]
+            })
+        },2000)
+    }
+})
+client.on("interactionCreate", async i => { 
+    if(!i.isButton()) return;
+    if(i.customId == "claim") { 
+        if(!i.member.roles.cache.has(mangement)) if(!i.member.permissions.has("ADMINISTRATOR")) return;
+        i.updete({
+            components: [new MessageActionRow().addComponents(new MessageButton().setCustomId("claim1").setLabel(i.user.displayName).setStyle("PRIMARY").setDisabled(true))]
+        }).then(() => {
+            db.add(`point-ticket${i.user.id}`, 1)
+            i.channel.setName(`${i.channel.name}/${i.user.displayName}`)
+        }).catch(() => 0)
+    
+    }
+})
+// اعطاء نقاط وسحبها : 
+client.on("messageCreate", async m => {
+    const message = m;
+
+    if(m.content.startsWith(prefix + "نقاط")) {
+        if(!m.member.roles.cache.has(mangement)) if(!m.member.permissions.has("ADMINISTRATOR")) return;
+        const user = m.mentions.members.first() || m.author
+        const point = db.fetch(`points${user.id}`) || 0
+        const ticket = db.fetch(`point-ticket${user.id}`) || 0
+        const ac = db.fetch(`point-ac${user.id}`) || 0
+        const game = db.fetch(`point-game${user.id}`) || 0
+        const embed = new MessageEmbed()
+        .setColor("YELLOW")
+        .setTitle(`استعلام النقاط :`)
+        .setDescription(`**
+    - نقاط تفعيل : 
+    \`${ac}\`
+    - نقاط التذاكر : 
+    \`${ticket}\`
+    - نقاط مضافه : 
+    \`${point}\`
+    - نقاط الاقيام : 
+    \`${game}\`
+    - مجموعهم : 
+    ${point + ac + ticket + game}
+              **`)
+      m.reply({embeds: [embed]})
+    }
+    if(m.content.startsWith(prefix + "اضافه-نقاط")) {
+        if(!m.member.roles.cache.has(perm)) if(!m.member.permissions.has("ADMINISTRATOR")) return;
+        const user = m.mentions.members.first()
+        const ar = message.content.split(' ').slice(2).join(" ")
+     
+        if(!user) return m.reply("منشن الاداري")
+       if(user === m.author) return m.reply("**لايمكنك اضافه نقاط بنفسك**")
+         if(isNaN(ar)) return m.reply("** اكتب النقاط بشكل صحيح**")
+       
+    db.add(`points${user.id}`, `${ar}`)
+          message.channel.send(`** ${message.author} تـم اضافه نقطة بـنجـاح**`)
+      
+    }
+    if(m.content.startsWith(prefix + "خصم-نقاط")) {
+        if(!m.member.roles.cache.has(perm)) if(!m.member.permissions.has("ADMINISTRATOR")) return;
+     const user = m.mentions.members.first()
+     const ar = message.content.split(' ').slice(2).join(" ")
+  
+     if(!user) return m.reply("منشن الاداري")
+    if(user === m.author) return m.reply("**لايمكنك سحب نقاط بنفسك**")
+      if(isNaN(ar)) return m.reply("** اكتب النقاط بشكل صحيح**")
+    
+ db.substr(`points${user.id}`, `${ar}`)
+       message.channel.send(`** ${message.author} تـم سحب نقطة بـنجـاح**`)
+   
+    }
+})
